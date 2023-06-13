be07bcac2f0fb1b1bec87e48e7a10b8575b1a289
+ <+>
2023-06-13 20:30:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+// Write your solution
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependscat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+   return cats.shift(name);
+}
+
+function appendCat(name){
+   return [...cats,name];
+}
+appendCat("Broom");
+
+    function prependCat(name) {
+     return [name,...cats] ;  
+}
+prependCat("Arnold");
+
+function removeLastCat(name){
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
+
+
+
+
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -11,21 +11,7 @@ describe('index.js', function () {
     beforeEach(function () {
       cats.length = 0;
 
-      cats.push('Milo', 'Otis', 'Garfield');
-    });
-
-    describe('destructivelyAppendCat(name)', function () {
-      it('appends a cat to the end of the cats array', function () {
-        destructivelyAppendCat('Ralph');
-
-        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
-      });
-    });
-
-    describe('destructivelyPrependCat(name)', function () {
-      it('prepends a cat to the beginning of the cats array', function () {
-        destructivelyPrependCat("Bob");
-
+l
         expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
       });
     });
@@ -78,4 +64,4 @@ describe('index.js', function () {
       });
     });
   });
-});
+
